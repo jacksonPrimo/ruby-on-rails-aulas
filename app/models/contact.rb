6a6f1acf92bc0c5171a0e80d068f64a4aa555de6
jacksonPrimo <jackson.primo12@gmail.com>
2024-04-09 22:50:51 -0300

    add same notes

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -9,6 +9,12 @@ class Contact < ApplicationRecord
     self.kind.description
   end
 
+  # o método render json dos controllers chama o método as_json do model, podemos
+  # sobreescrever este método passando options no seu argumento, chamando o super
+  # para manter o chamada original e neste caso, estamos passando uma configuração
+  # padrão para o método, que seria renderizar o método author e kind_description
+  # assim como colocar todos os atributos dentro de uma chave com o nome do próprio
+  # model com o root: true
   def as_json(options={})
     super(
       methods: [:author, :kind_description], 
--- app/models/kind.rb
+++ app/models/kind.rb
@@ -1,2 +1,5 @@
+# é importante lembrar que em casos onde há o uso de migrations, não precisamos
+# especificar dentro dos models os campos que ele possui, normalmente apenas
+# sobrescrevemos alguns métodos e criamos nóvos para o model
 class Kind < ApplicationRecord
 end
