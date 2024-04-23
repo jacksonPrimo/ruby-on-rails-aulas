5024376fb5f49d60f19dddacd743f86b2b16b597
jacksonPrimo <jackson.primodev@gmail.com>
2024-04-22 21:12:55 -0300

    add phone by generate model cli

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_16_180334) do
+ActiveRecord::Schema[7.0].define(version: 2024_04_23_000808) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -30,5 +30,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_16_180334) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "phones", force: :cascade do |t|
+    t.string "number"
+    t.bigint "contact_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["contact_id"], name: "index_phones_on_contact_id"
+  end
+
   add_foreign_key "contacts", "kinds"
+  add_foreign_key "phones", "contacts"
 end
